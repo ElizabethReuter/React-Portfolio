750145b3cfdc1b42339cb8096e64bd730a806022
Elizabeth Reuter <elizabeth.j.reuter@gmail.com>
2020-11-23 22:30:26 -0600

    fixing bugs

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -4,20 +4,20 @@ import { Link } from "react-router-dom";
 function Nav() {
 
   return (
-    <nav class="navbar navbar-expand-lg navbar-light nav-align-custom">
-        <a class="navbar-brand" href="https://www.linkedin.com/in/elizabeth-reuter-2132411b0/" target="_blank">Elizabeth Reuter</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+    <nav className="navbar navbar-expand-lg navbar-light nav-align-custom">
+        <a className="navbar-brand" href="https://www.linkedin.com/in/elizabeth-reuter-2132411b0/" target="_blank">Elizabeth Reuter</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item active">
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item active">
               <Link to="home">About</Link>
             </li>
-            <li class="nav-item">
+            <li className="nav-item">
               <Link to="portfolio">Portfolio</Link>
             </li>
-            <li class="nav-item">
+            <li className="nav-item">
               <Link to="contact">Contact</Link>
             </li>
           </ul>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
         <div className="col-md-6 col-sm-3">
           <h1>About Me</h1>
           <hr/>
-            <img className="img" src="IMG_0045.jpeg" alt="Me and my dog" className="img-thumbnail"/>
+            <img className="img" src="IMG_0045.jpeg" alt="Photo" className="img-thumbnail"/>
                 <p>Elizabeth is a travel professional with over 10 years of sales and customer service experience. She has decided to make the move from the corporate travel industry to a coding and web development program due to the Covid-19 pandemic.</p>
                 <p>She graduated from Western Michigan University in 2014 with a BA in Travel and Tourism Marketing. She have been living in Lakeview, Chicago for the past 5 years. She has a two year old Golden Retriever named Gretchen that she spends the majority of her free time with.</p>
                  <p>Elizabeth is originally from Grand Rapids, MI. She has also lived in Kalamazoo, MI and Dublin, Ireland for a short period of time. She is excited to learn coding and development to expand her skill set.</p>
